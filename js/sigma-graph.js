7b58355a6b713c271bddd07b2d92637f2634b932
mydu <dumengying1989@gmail.com>
2016-07-21 16:29:11 +0200

    modify neighbour edges when click node

--- js/sigma-graph.js
+++ js/sigma-graph.js
@@ -1,6 +1,7 @@
 var g = $graph_data ;
 
 s = new sigma({graph: g, container: '$container', settings: { defaultNodeColor: '#ec5148'} });
+// var filter=new sigma.plugins.filter(s);
 
 s.graph.nodes().forEach(function(n) {
   n.originalColor = n.color;
@@ -8,7 +9,9 @@ s.graph.nodes().forEach(function(n) {
 s.graph.edges().forEach(function(e) {
   e.originalColor = e.color;
 });
+
 s.startForceAtlas2({worker: true, barnesHutOptimize: false});
+
 s.bind('clickNode', function(e) {
   var nodeId = e.data.node.id,
       toKeep = s.graph.neighbors(nodeId);
@@ -22,12 +25,14 @@ s.bind('clickNode', function(e) {
   });
 
   s.graph.edges().forEach(function(e) {
-    if (toKeep[e.source] && toKeep[e.target])
+    if ((toKeep[e.source] && e.target===nodeId)||(e.source===nodeId && toKeep[e.target]))
       e.color = e.originalColor;
     else
       e.color = '#eee';
   });
-
+  // filter.undo("neighbors")
+  //             .neighborsOf(nodeId,"neighbors")
+  //             .apply();
   s.refresh();
 });
 
@@ -39,6 +44,6 @@ s.bind('clickStage', function(e) {
   s.graph.edges().forEach(function(e) {
     e.color = e.originalColor;
   });
-
+  // filter.undo("neighbors")
   s.refresh();
 });
